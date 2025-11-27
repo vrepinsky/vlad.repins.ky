06161680de7c2480558a0b8e4be2744f37be7f73
Vlad Repinsky <devrepinsky@gmail.com>
2025-11-27 14:26:55 +0100

    Remove the vertical line in cv history

--- src/components/cv/CVEntryList.tsx
+++ src/components/cv/CVEntryList.tsx
@@ -8,7 +8,6 @@ export const CVEntryList = ({ entries }: { entries: CVEntry[] }) => {
       {entries.map((entry, index) => (
         <EntryWrapper key={entry.company + entry.title}>
           <CvEntry entry={entry} />
-          {index < entries.length - 1 && <VerticalLine />}
         </EntryWrapper>
       ))}
     </ListContainer>
@@ -28,15 +27,4 @@ const EntryWrapper = styled("div")`
   width: 100%;
   max-width: 512px;
   margin-bottom: 4rem;
-`;
-
-const VerticalLine = styled("div")`
-  position: absolute;
-  left: 100px;
-  top: 100%;
-  height: 4rem;
-  width: 2px;
-  border-left: 1px solid ${(props) => props.theme.palette.text};
-  opacity: 0.3;
-  z-index: 1;
 `;
