b37d236b190524c340239be04a17711b48295126
Vlad Repinsky <devrepinsky@gmail.com>
2026-02-12 17:52:36 +0100

    Reorganise categories ^_____^

--- src/constants/wishlist.constant.ts
+++ src/constants/wishlist.constant.ts
@@ -2,85 +2,92 @@ import type { WishlistSection } from "@/types/wishlist.types";
 
 export const WISHLIST_SECTIONS: WishlistSection[] = [
   {
-    heading: "Clothes",
-    items: [
-      { title: "Shyne Hoodie", link: "https://shyneteam.com/collections/shyne", price: 129 },
-      {
-        title: "Ich <3 betrunken E-roller Fahren",
-        link: "https://versagerclo.de/products/betrunken-e-roller-fahren-t-shirt",
-        price: 25
-      },
-      {
-        title: "Original Muay Thai Shorts Silver",
-        link: "https://eu.yokkao.com/de/products/original-muay-thai-shorts-silver",
-        price: 70
-      },
-      {
-        title: "England jacket Union Jack Plaid Zipup",
-        link: "https://shopjogabonito.com/products/union-jack-plaid-zipup",
-        price: 100
-      },
-      {
-        title: "Collective Grappling Shorts",
-        link: "https://moribound.com/products/jiu-jitsu-collective-grappling-shorts",
-        price: 40
-      },
-      {
-        title: "Jackets, shirts and more from Universal Surplus",
-        link: "https://universal-surplus.com/products/ffa-newyork-jacket",
-        price: 100
-      },
-    ],
-  },
-  {
-    heading: "House",
+    heading: "Entrées",
     items: [
       {
-        title: "Vintage & Modern Lamp 01",
-        link: "https://kismas.com/collections/vintage-modern-lamp-01",
-        price: 150
-      },
-    ],
-  },
-  {
-    heading: "Misc",
-    items: [
-      {
-        title: "Mezcal Sacrificio Reposado",
-        link: "https://www.totalwine.com/spirits/mezcal/mezcal-sacrificio-reposado/p/141213750",
-        price: 40
-      },
-      {
-        title: "Kinnie Zero",
-        link: "https://maltaladen.de/product/24-kinnie-zero-pet-softdrink/",
-        price: 40
+        title: "Drive DVD by NWR",
+        link: "https://bynwr.com/shop/drive-nordic-dvd",
+        price: 10,
       },
       {
         title: "Drinks from Apfelweinkontor",
         link: "https://www.apfelweinkontor.com/online-shop/",
-        price: 20
+        price: 20,
+      },
+    ],
+  },
+  {
+    heading: "Les Plats Principaux",
+    items: [
+      {
+        title: "Ich <3 betrunken E-roller Fahren",
+        link: "https://versagerclo.de/products/betrunken-e-roller-fahren-t-shirt",
+        price: 25,
       },
       {
-        title: "Apple iMac G4 Sunflower 17 inch",
-        link: "https://www.marktplaats.nl/v/computers-en-software/apple-desktops/m2363904367-apple-imac-g4-sunflower-17-inch-werkend-designklassieker",
-        price: 150
+        title: "Collective Grappling Shorts",
+        link: "https://moribound.com/products/jiu-jitsu-collective-grappling-shorts",
+        price: 40,
       },
       {
-        title: "Drive DVD by NWR",
-        link: "https://bynwr.com/shop/drive-nordic-dvd",
-        price: 10
+        title: "Mezcal Sacrificio Reposado",
+        link: "https://www.totalwine.com/spirits/mezcal/mezcal-sacrificio-reposado/p/141213750",
+        price: 40,
+      },
+      {
+        title: "Kinnie Zero",
+        link: "https://maltaladen.de/product/24-kinnie-zero-pet-softdrink/",
+        price: 40,
       },
       {
         title: "Vinyl groove of ESSR",
         link: "https://vinylpladen.de/vinyl/various-artists/groove-of-essr-funk-disco-jazz-from-soviet-estonia-LP",
-        price: 40
+        price: 40,
       },
-      { title: "Gotthard Base Tunnel Tour" },
+    ],
+  },
+  {
+    heading: "Les Desserts",
+    items: [
+      {
+        title: "Original Muay Thai Shorts Silver",
+        link: "https://eu.yokkao.com/de/products/original-muay-thai-shorts-silver",
+        price: 70,
+      },
+      {
+        title: "England jacket Union Jack Plaid Zipup",
+        link: "https://shopjogabonito.com/products/union-jack-plaid-zipup",
+        price: 100,
+      },
+      {
+        title: "Jackets, shirts and more from Universal Surplus",
+        link: "https://universal-surplus.com/products/ffa-newyork-jacket",
+        price: 100,
+      },
+      {
+        title: "Shyne Hoodie",
+        link: "https://shyneteam.com/collections/shyne",
+        price: 129,
+      },
+      {
+        title: "Vintage & Modern Lamp 01",
+        link: "https://kismas.com/collections/vintage-modern-lamp-01",
+        price: 150,
+      },
+      {
+        title: "Apple iMac G4 Sunflower 17 inch",
+        link: "https://www.marktplaats.nl/v/computers-en-software/apple-desktops/m2363904367-apple-imac-g4-sunflower-17-inch-werkend-designklassieker",
+        price: 150,
+      },
       {
         title: "Amber Eau de Parfum Soie Malaquais",
         link: "https://www.driesvannoten.com/en-de/products/001-099009",
-        price: 300
+        price: 300,
       },
     ],
+  },
+  {
+    heading: "Suggestions du chef",
+    items: [{ title: "Gotthard Base Tunnel Tour" }],
   },
 ];
