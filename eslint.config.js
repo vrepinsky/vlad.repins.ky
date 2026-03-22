26849c04ad58225dad433644caed05fdce4db6e9
Vlad Repinsky <vladrepinsky@gmail.com>
2026-03-22 20:22:04 +0100

    Complete migration from react

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,16 @@
 import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactPlugin from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 
 export default defineConfig([
   {
-    ignores: [
-      ".astro/**",
-      "dist/**",
-      "node_modules/**",
-      "src/frontend.tsx",
-      "src/components/**/*.tsx",
-      "src/context/**",
-      "src/hooks/**",
-      "src/utils/**",
-    ],
+    ignores: [".astro/**", "dist/**", "node_modules/**"],
   },
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  reactPlugin.configs.flat.recommended,
-  reactPlugin.configs.flat["jsx-runtime"],
   {
-    files: ["**/*.{ts,tsx,js,jsx}"],
-    plugins: {
-      react: reactPlugin,
-      "react-hooks": reactHooks,
-    },
-    settings: {
-      react: { version: "detect" },
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-    },
+    files: ["**/*.{ts,js,mjs}"],
   },
   eslintConfigPrettier,
 ]);
--- src/hooks/useImageViewer.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from "react";
-import { ImageViewerContext } from "../context/imageviewer.provider";
-
-export const useImageViewer = () => {
-  return useContext(ImageViewerContext);
-};
