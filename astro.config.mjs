401a5d4a8169fd356b527a7ec0d51fc4a870dc85
Vlad Repinsky <vladrepinsky@gmail.com>
2026-03-22 19:30:09 +0100

    Add inline style sheet configuration in Astro setup; enhance global styles with new padding variables and improve layout responsiveness. Update Location component for better accessibility and introduce font preloading in BaseLayout.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://vlad.repins.ky",
   output: "static",
+  build: {
+    inlineStyleSheets: "always",
+  },
   integrations: [sitemap()],
 });
