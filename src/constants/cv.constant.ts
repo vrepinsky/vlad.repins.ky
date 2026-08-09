29e873d6ce4bcbeb45e8fca524cffff7fea74c8e
Vlad Repinsky <vladrepinsky@gmail.com>
2026-08-09 20:56:24 +0200

    Add sparqle.com link to the Sparqle CV entry.

--- src/constants/cv.constant.ts
+++ src/constants/cv.constant.ts
@@ -10,6 +10,7 @@ export const WORK_EXPERIENCE: CVEntry[] = [
       "We're a small team building an accumulator network of green delivery companies across Western Europe. We're solving problems of limited observability and human error through engineering, to improve the UX of a key service like package delivery. Forward-deployed engineering at its finest.",
     ],
     location: "Amsterdam, Netherlands",
+    link: "https://sparqle.com",
   },
   {
     startDate: "May 2025",
